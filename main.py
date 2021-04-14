1742960406ce9564e9ff0b2d0af3bcc1a5688927
Eduardo Margulis <edumarg@gmx.com>
2021-04-14 12:27:27 +0300

    improved wind message with cardinal direction

--- main.py
+++ main.py
@@ -13,16 +13,34 @@ def get_weather_info(city):
         return
     if response.status_code == 200:
         data = json.loads(response.content)
-        print(f'Current Weather at {city.capitalize()}: \n' \
-              f'{data["weather"][0]["description"].capitalize()} \n' \
+        if 0 <= data["wind"]["deg"] < 45:
+            cardinal = "N"
+        elif 45 <= data["wind"]["deg"] < 90:
+            cardinal = "NE"
+        elif 90 <= data["wind"]["deg"] < 135:
+            cardinal = "E"
+        elif 135 <= data["wind"]["deg"] < 180:
+            cardinal = "SE"
+        elif 180 <= data["wind"]["deg"] < 225:
+            cardinal = "SW"
+        elif 225 <= data["wind"]["deg"] < 270:
+            cardinal = "W"
+        elif 270 <= data["wind"]["deg"] < 315:
+            cardinal = "NW"
+        elif 315 <= data["wind"]["deg"] < 360:
+            cardinal = "N"
+        print(f'Current Weather at {city.capitalize()}: \n'\
+              f'{data["weather"][0]["description"].capitalize()} \n'\
               f'Current Temperature: {data["main"]["temp"]}°C \n'\
               f'Temperature feels like: {data["main"]["feels_like"]}°C \n'\
-              f'Max Temperature: {data["main"]["temp_max"]}°C \n' \
-              f'Min Temperature: {data["main"]["temp_min"]}°C \n' \
-              f'Humidity: {data["main"]["humidity"]}% \n' \
-              f'Pressure: {data["main"]["pressure"]}hPa \n'
+              f'Max Temperature: {data["main"]["temp_max"]}°C \n'\
+              f'Min Temperature: {data["main"]["temp_min"]}°C \n'\
+              f'Humidity: {data["main"]["humidity"]}% \n'\
+              f'Pressure: {data["main"]["pressure"]}hPa \n'\
               f'Visibility: {data["visibility"]}mts. \n'\
-              f'Wind: Speed{data["wind"]["speed"]}Km/h, Direction{data["wind"]["deg"]}°')
+              f'Wind: \n'\
+              f'\tSpeed {data["wind"]["speed"]}Km/h \n'
+              f'\tDirection {data["wind"]["deg"]}° {cardinal}')
         return
     if response.status_code == 429:
         print("Minute request limit reached...")
