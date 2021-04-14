1da1a782a6b6e72de9a9baa39ade96f31547fb85
Eduardo Margulis <edumarg@gmx.com>
2021-04-14 13:10:52 +0300

    improved display of units for various messurements

--- main.py
+++ main.py
@@ -24,21 +24,21 @@ def get_weather_info(city_name):
 
 def get_wind_cardinal_direction(data):
     cardinal = ""
-    if 0 <= data["wind"]["deg"] < 45:
+    if 0 <= data["wind"]["deg"] < 33.75:
         cardinal = "N"
-    elif 45 <= data["wind"]["deg"] < 90:
+    elif 33.75 <= data["wind"]["deg"] < 78.75:
         cardinal = "NE"
-    elif 90 <= data["wind"]["deg"] < 135:
+    elif 78.75 <= data["wind"]["deg"] < 123.75:
         cardinal = "E"
-    elif 135 <= data["wind"]["deg"] < 180:
+    elif 123.75 <= data["wind"]["deg"] < 213.75:
         cardinal = "SE"
-    elif 180 <= data["wind"]["deg"] < 225:
+    elif 213.75 <= data["wind"]["deg"] < 258.75:
         cardinal = "SW"
-    elif 225 <= data["wind"]["deg"] < 270:
+    elif 258.75 <= data["wind"]["deg"] < 303.75:
         cardinal = "W"
-    elif 270 <= data["wind"]["deg"] < 315:
+    elif 303.75 <= data["wind"]["deg"] < 348.75:
         cardinal = "NW"
-    elif 315 <= data["wind"]["deg"] < 360:
+    elif 348.75 <= data["wind"]["deg"] < 360:
         cardinal = "N"
     return cardinal
 
@@ -55,9 +55,9 @@ def print_weather_info(city_name):
               f'Min Temperature: {data["main"]["temp_min"]}°C \n' \
               f'Humidity: {data["main"]["humidity"]}% \n' \
               f'Pressure: {data["main"]["pressure"]}hPa \n' \
-              f'Visibility: {data["visibility"]}mts. \n' \
+              f'Visibility: {data["visibility"]/1000:.2f}Km. \n' \
               f'Wind: \n' \
-              f'\tSpeed: {data["wind"]["speed"]}Km/h \n'
+              f'\tSpeed: {data["wind"]["speed"]*1.60934:.2f}Km/h \n'
               f'\tDirection: {data["wind"]["deg"]}° {cardinal}')
     else:
         return
